6621f563e74fd4160b434c5968c4a57fa361ebee
Daniel Nolan <DanNolan99@gmail.com>
2020-07-14 12:54:14 -0400

    send over the message

--- sign.js
+++ sign.js
@@ -3,7 +3,7 @@ const SHA256 = require('crypto-js/sha256');
 
 const ec = new EC('secp256k1');
 
-const privateKey = '393f2aa9eb8099bc63924d36b3435f0b1098c69a7c3106525102ce0b98fea723';
+const privateKey = 'a62a36fdeebbe9befdbeb3111d526c9aa2fbb048e1601330e71b65659d3a31b8';
 
 const key = ec.keyFromPrivate(privateKey);
 
@@ -14,7 +14,7 @@ const msgHash = SHA256(message);
 const signature = key.sign(msgHash.toString());
 
 console.log({
-  msgHash: msgHash.toString(),
+  message,
   signature: {
     r: signature.r.toString(16),
     s: signature.s.toString(16)
--- verify.js
+++ verify.js
@@ -1,18 +1,20 @@
 const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
 
 const ec = new EC('secp256k1');
 
 const publicKey = {
-  x: '7a37a85866eaa585f0ce0b5d4e3a6104871e91f62c61a5820383e59aa64063d',
-  y: 'cdbf6b76837edcefa6ca248e06ecb5b4d7de2d3078b52c36e5fc43082cda8530'
+  x: '833ef084ef9ebd54ee0846da02abda5075f048ad7c5b078380f0937944e462d8',
+  y: '555dcd284db4f67e2a61441a74063910452c03eacaddac9a81ff8bd171948be9'
 }
 
 const key = ec.keyFromPublic(publicKey, 'hex');
 
-const msgHash = "7fe4cd31aa57248eda0384fcc7d007fc042bd4927a1d6c50e6d03cccbec8d394";
+const msg = "I am in the ChainShot Bootcamp";
+const msgHash = SHA256(msg).toString();
 const signature = {
-  r: 'f7d2f1688b2027ac56b57d829db3cbe5418f27ffb527dde16b64949e8f0c9489',
-  s: '84a9b3b98450a05bef022f21ca53ea6ec7dc1dd3dfa8358c4647262ab96afa32'
+  r: '8dc5b6cc4d02c7d01fcf39020f08f920b5a302180f7bb62f40d937b937b99a95',
+  s: 'fa9b4009ab83fa091155505c9d08a14ac56259f02229b7672a3678a762e3c091'
 };
 
 console.log(key.verify(msgHash, signature));
